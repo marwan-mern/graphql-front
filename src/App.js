bcad7e801ae7fa800f855f5c8a4d0bd2e2533e02
marwan-mern <118802926+marwan-mern@users.noreply.github.com>
2022-11-22 14:02:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: 'https://marwan-graphql-app.onrender.com',
+  uri: 'https://marwan-graphql-app.onrender.com/graphql',
   cache,
 });
 
